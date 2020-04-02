32dfb8bbd3d8b186bdccdc60ee0487efbfa9cdee
wtam26 <wtam26@terpmail.umd.edu>
2020-04-02 02:53:45 -0400

    Converting from HTML to Javascript to populate thumbnail bar

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -6,9 +6,12 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
+/* Willie 1st edition - currently testing to see how to append image into thumbBar */
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src', 'images/pic1.jpg');
 thumbBar.appendChild(newImage);
+
+
 
 /* Wiring up the Darken/Lighten button */
 
