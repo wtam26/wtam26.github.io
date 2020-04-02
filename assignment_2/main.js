283335a24cdb2db012faf41547226e24d0a92496
wtam26 <wtam26@terpmail.umd.edu>
2020-04-02 02:55:37 -0400

    Update main.js

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -11,6 +11,12 @@ const newImage = document.createElement('img');
 newImage.setAttribute('src', 'images/pic1.jpg');
 thumbBar.appendChild(newImage);
 
+/* Willie 2nd edition - too ineffective, but keeping it here for testing purposes for Looping through images*/
+const newImage2 = document.createElement('img');
+newImage2.setAttribute('src', 'images/pic2.jpg');
+thumbBar.appendChild(newImage2);
+
+
 
 
 /* Wiring up the Darken/Lighten button */
