929ea773e08bc42cc377954c182c44b1764ef27f
wtam26 <wtam26@terpmail.umd.edu>
2020-04-02 03:55:26 -0400

    Finished Lighten/Darken function

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -43,11 +43,11 @@ btn.onclick = function() {
     if (btnClass === 'dark') {
         btn.setAttribute('class', 'light');
         btn.textContent = 'Lighten';
-        //overlay.style.backgroundColor = rgba(0,0,0,0.5);
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
     } else {
         btn.setAttribute('class', 'dark');
         btn.textContent = 'Darken';
-        //overlay.style.backgroundColor = rgba(0,0,0,0);
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
 
     
