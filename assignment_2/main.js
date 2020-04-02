3981281c3a087ca437f618ba1e6d8f85ddab638a
wtam26 <wtam26@terpmail.umd.edu>
2020-04-02 02:57:36 -0400

    Update main.js

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -6,15 +6,21 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
+
+
 /* Willie 1st edition - currently testing to see how to append image into thumbBar */
+
+/*Willie 3rd edition - went back to read ELMS assignment and currently using a loop*/
+
 const newImage = document.createElement('img');
 newImage.setAttribute('src', 'images/pic1.jpg');
 thumbBar.appendChild(newImage);
 
-/* Willie 2nd edition - too ineffective, but keeping it here for testing purposes for Looping through images*/
+/* Willie 2nd edition - too ineffective, but keeping it here for testing purposes for 'Looping through images' until thumbnail bar can be populated
 const newImage2 = document.createElement('img');
 newImage2.setAttribute('src', 'images/pic2.jpg');
-thumbBar.appendChild(newImage2);
+thumbBar.appendChild(newImage2); */
+
 
 
 
